3caca0f8fd3d2c23ee1b6e90a1be65c82c0429a2
KostenkoVeronika <kostenkoveronika@outlook.com>
2018-05-01 16:34:15 +1000

    validation

--- js/pictures.js
+++ js/pictures.js
@@ -326,3 +326,115 @@ pictureBigCancel.addEventListener('click', function() {
   modalOpenRemoveHandler();
   document.removeEventListener('keydown', pictureCloseEscHandler);
 });
+
+// ТЗ - ВАЛИДАЦИЯ
+
+var form = document.querySelector('.img-upload__form');
+var hashtag = form.querySelector('.text__hashtags');
+var formSubmit = form.querySelector('#upload-submit');
+var comment = form.querySelector('.text__description');
+
+// обработчики
+var hashtagValidateHandler = function (evt) {
+  var hashtagValue = hashtag.value;
+  var hashtagArray = hashtagValue.split(' ');
+  var sameHashtag = false;
+  var hashtagError = 'Вы неправильно ввели данные! ';
+  for (var i = 0; i < hashtagArray.length; i++) {
+    var hashtagItem = hashtagArray[i];
+    var firstSymbol = hashtagItem.charAt(0);
+    var itemLength = hashtagItem.length;
+    var doubleHashtag = false;
+    var sameCount = 0;
+    // проверка пробелов (встречается ли в хэштеге еще одна решетка)
+    for (var j = 1; j < hashtagItem.length; j++) {
+      if (hashtagItem.charAt(j) === '#') {
+        doubleHashtag = true;
+      }
+    }
+    // проверка на повторяющиеся хештеги
+    if (sameHashtag === false) {
+      for (var k = 0; k < hashtagArray.length; k++) {
+        if (hashtagItem.toLowerCase() === hashtagArray[k].toLowerCase()) {
+          sameCount += 1;
+        }
+        if (sameCount >= 2) {
+          sameHashtag = true;
+        }
+      }
+    }
+    // формирует сообщение об ошибках
+    if (hashtagItem !== '') {
+      if (firstSymbol !== '#') {
+        hashtagError += 'Хэш-тег должен начинаться с символа #. ';
+      }
+      if (hashtagItem === '#') {
+        hashtagError += 'Хэш-тег не может состоять только из символа #. ';
+      }
+      if (itemLength > 20) {
+        hashtagError += 'Длина одного хэш-тега не должна превышать 20 символов. ';
+      } 
+      if (doubleHashtag) {
+        hashtagError += 'Хэш-теги должны разделяться пробелами. ';
+      }
+      if (hashtagArray.length > 5) {
+        hashtagError += 'Хэш-тегов не может быть больше 5 штук. ';
+      }
+      if (sameHashtag) {
+        var sameHashtagMessage = 'Хэш-теги не должны повторяться. ';
+        // проверяет есть ли уже сообщение о повторении хэштега
+        if (hashtagError.indexOf(sameHashtagMessage) === -1){
+          hashtagError += sameHashtagMessage;
+        }
+      }  
+    }
+  }
+  // выводит сообщение об ошибках
+  if (hashtagError !== 'Вы неправильно ввели данные! ') {
+    hashtag.setCustomValidity(hashtagError);
+    hashtag.classList.add('description-invalid');
+  } else {
+    hashtag.setCustomValidity('');
+    hashtag.classList.remove('description-invalid');
+  }
+};
+
+var commentValidateHandler = function (evt) {
+  var commentValue = comment.value;
+  var commentError = 'Вы неправильно ввели комментарий! ';
+  if (commentValue !== '') {
+    if (commentValue.length > 140) {
+      commentError += 'Комментрий не должен быть длиннее 140 символов. '
+    }
+  }
+  if (commentError !== 'Вы неправильно ввели комментарий! ') {
+    comment.setCustomValidity(commentError);
+    comment.classList.add('description-invalid');
+  } else {
+    comment.setCustomValidity('');
+    comment.classList.remove('description-invalid');
+  }
+};
+
+// при клике на сабмит валидирует хэштеги и комментарий
+formSubmit.addEventListener('click', function (evt) {
+  hashtagValidateHandler(evt);
+  commentValidateHandler(evt);
+});
+// когда фокус на хэштегах клавиша еск не сработает
+hashtag.addEventListener('focus', function () {
+  document.removeEventListener('keydown', pictureChangeCloseEscHandler);
+});
+// когда фокус ушёл с хэштегов клавиша еск сработает
+hashtag.addEventListener('blur', function () {
+  document.addEventListener('keydown', pictureChangeCloseEscHandler);
+});
+// когда фокус на комментах клавиша еск не сработает
+comment.addEventListener('focus', function () {
+  document.removeEventListener('keydown', pictureChangeCloseEscHandler);
+});
+// когда фокус ушёл с комментов клавиша еск сработает
+comment.addEventListener('blur', function () {
+  document.addEventListener('keydown', pictureChangeCloseEscHandler);
+});
+
